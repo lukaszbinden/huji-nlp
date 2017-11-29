35954c891260a443c5c7e72bfa28c3ec943b8fed
Lukas Zbinden <lukas.zbinden@unifr.ch>
2017-11-29 09:22:28 +0200

    finish functions train_eml, eml, train_qml, qml

--- Exercise_02/exercise_2_c.py
+++ Exercise_02/exercise_2_c.py
@@ -13,17 +13,42 @@ test_set = news_sents[set_size:]
 # Training phase: Compute the transition and emission probabilities of a bigram HMM tagger
 # directly on the training set using maximum likelihood estimation.
 
-def eml(xi, yi, train_set, deml):
+def train_eml(train_set):
     """
-    Computes emission probability q(x_i | y_i) using maximum likelihood estimation on the training set.
+    Computes emission probabilities e(x_i | y_i) using maximum likelihood estimation on the training set.
+    :param train_set: the training set
+    :return deml: dictionary for e where computed e(y_i | y_i-1) values are stored
+    """
+    size_of_set = len(train_set)
+    deml = defaultdict(Counter)
+    for i in range(size_of_set):
+        sent = train_set[i]
+        prior = '*'
+        for word, tag in sent:
+            deml[tag][word] +=1
+            prior = tag
+        deml[prior]['STOP'] +=1
+    return deml
+
+
+def eml(xi, yi, deml):
+    """
+    Computes emission probability e(x_i | y_i) using maximum likelihood estimation on the training set.
     :param xi: a word x_i
     :param yi: a label/state y_i
     :param train_set: the training set
     :param deml: dictionary for eml where pre-computed values are available
     :return: q(y_i | y_i-1)
     """
+    return deml[yi][xi] / sum(deml[yi].values())
+
 
 def train_qml(train_set):
+    """
+    Computes transition probabilities q(y_i | y_i-1) using maximum likelihood estimation on the given training set.
+    :param train_set: the training set
+    :return dqml: dictionary for qml where computed q(y_i | y_i-1) values are stored
+    """
     size_of_set = len(train_set)
     dqml = defaultdict(Counter)
     for i in range(size_of_set):
@@ -40,36 +65,33 @@ def qml(yi, yi1, dqml):
     Computes transition probability q(y_i | y_i-1) using maximum likelihood estimation on the training set.
     :param yi: a label/state y_i
     :param yi1: a label/state y_i-1
-    :param train_set: the training set
-    :param dqml: dictionary for qml where pre-computed values are available
-    :return: q(y_i | y_i-1)
+    :param dqml: dictionary for qml where pre-computed values are stored
+    :return: qml(y_i | y_i-1)
     """
-
-    # if yi1 not in dqml:
-    #     size_of_set = len(train_set)
-    #     for i in range(size_of_set):
-    #         sent = train_set[i]
-    #         prior = '*'
-    #         for word, tag in sent:
-    #             dqml[prior][tag] +=1
-    #             prior = tag
-    #         dqml[prior]['STOP'] +=1
-
     return dqml[yi1][yi] / sum(dqml[yi1].values())
 
 
 #dqml = {}  # dict
 #dqml = defaultdict(Counter)
-dqml = train_qml(train_set)
-qnptl = qml('NN', 'AT', dqml)
-print('qnptl: ', qnptl)
-print('exp: ', 12/19)
+# dqml = train_qml(train_set)
+# qnptl = qml('NN', 'AT', dqml)
+# print('qnptl: ', qnptl)
+# print('exp: ', 12/19)
+#
+# qnptl = qml('AT', 'AT', dqml)
+# print('qnptl: ', qnptl)
+# print('exp: 0.0')
 
-qnptl = qml('AT', 'AT', dqml)
-print('qnptl: ', qnptl)
-print('exp: 0.0')
-
-
+deml = train_eml(train_set)
+print('-------------')
+for i in deml:
+    print(i, '---', deml[i])
+    if i == 'AT':
+        print(deml[i]['The'])
+        print(sum(deml[i].values()))
+        print(eml('The', i, deml))
+        print('exp: ', 4/19)
+        print(eml('hebedi', i, deml))
 
 #for sent in train_set:
 #    print(sent)
