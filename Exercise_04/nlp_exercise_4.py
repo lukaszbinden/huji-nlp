5da156cddc0ee62f92fa02d15d5fcd9ee66f838b
Lukas Zbinden <lukas.zbinden@unifr.ch>
2018-01-18 15:07:27 +0200

    do 3.a)

--- Exercise_04/nlp_exercise_4.py
+++ Exercise_04/nlp_exercise_4.py
@@ -11,7 +11,30 @@ def extractor_proper_nouns(wikipedia_page):
     nlp_model = spacy.load('en')
     analyzed_page = nlp_model(page)
 
-    result = [w.text for w in analyzed_page if w.pos_ == 'PROPN']
+    result = []
+    for i in range(len(analyzed_page)):
+        if i < len(analyzed_page) - 1:
+            w = analyzed_page[i]
+            if w.pos_ == 'PROPN':
+                word = w.text
+                index = 1
+                stop = False
+                while True:
+                    if i + index < len(analyzed_page) - 1:
+                        w1 = analyzed_page[i + index]
+                        if w1.pos_ == 'PROPN':
+                            word = word + ' ' + w1.text
+                            index = index + 1
+                        else:
+                            break
+                else:
+                    stop = True
+                result.append(word)
+                if stop:
+                    break
+
+
+    # result = [w.text for w in analyzed_page if w.pos_ == 'PROPN']
 
     return result
 
